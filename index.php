131ab4d474d5f235958f4e9db6f0668b2bef77c0
CiceroLucas <lukasferreiradesousa890@gmail.com>
2020-11-18 10:16:30 -0300

    criando a section one

--- index.php
+++ index.php
@@ -5,23 +5,15 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>BodyFitness</title>
 	<link rel="stylesheet" type="text/css" href="css/estilo.css">
-
 </head>
 <body>
 
 
-	<nav> 
-        <label class="logo">BODYFITNESS</label>
-        <ul>
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="sobre.php">Sobre Nós</a></li>
-            <li><a href="time.php">Nosso Time</a></li>
-    	    <li><a href="serviços.php">Serviços</a></li>
-	    	<li><a href="contato.php">Contato</a></li>
-        </ul>
-    </nav>
 
 <section class="wallpaper"></section>
+
+
+
 
 
 
---
+++ section_one.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>BodyFitness</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+
+</head>
+<body>
+
+      <section id="one">
+            <h3>Porquê nos escolher?</h3>
+            <h1>Empurre seus limites para a frente</h1>
+
+            <div class="icons">
+                <figure>
+                    <img src="img/icon.png">
+                    <figcaption>????????????</figcaption>
+                </figure>
+                <figure>
+                    <img src="img/icon.png">
+                    <figcaption>????????????</figcaption>
+                </figure>
+                <figure>
+                    <img src="img/icon.png">
+                    <figcaption>????????????</figcaption>
+                </figure>
+                <figure>
+                    <img src="img/icon.png">
+                    <figcaption>????????????</figcaption>
+                </figure>
+            </div>
+      </section>
+
+</body>
+</html>
