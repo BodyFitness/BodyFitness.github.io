1ab19789784e43e1d385ff1a16342f27acae141e
Marcos Lorhan <marcoslorhanbs@gmail.com>
2020-11-23 10:17:10 -0300

    Prior Changes in navbar using bootstrap

--- navbar.php
+++ navbar.php
@@ -3,20 +3,31 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="css/estilo.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
 </head>
 <body>
 
-	<nav> 
-        <label class="logo">BODYFITNESS</label>
-        <ul>
-            <li><a href="index.php">Inicio</a></li>
-            <li><a href="sobre.php">Sobre Nós</a></li>
-            <li><a href="time.php">Nosso Time</a></li>
-    	    <li><a href="serviços.php">Serviços</a></li>
-	    	<li><a href="contato.php">Contato</a></li>
+	<div class="navbar navbar-expand-lg bg-warning"> 
+        <a class="navbar-brand">BODYFITNESS</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="sobre.php">Sobre Nós</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="time.php">Nosso Time</a>
+            </li>
+    	    <li class="nav-item">
+                <a class="nav-link" href="serviços.php">Serviços</a>
+            </li>
+	    	<li class="nav-item">
+                <a class="nav-link" href="contato.php">Contato</a>
+            </li>
         </ul>
-    </nav>
+    </div>
 
 </body>
 </html>
